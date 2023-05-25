9b9f3320fefac8713c74eb95e3a6ccff1a6e4baf
Sivakumar D <sivakumar@topaztech.in>
2023-05-25 14:25:58 +0530

    Name changed

--- HelloWorld/Controllers/WeatherForecastController.cs
+++ HelloWorld/Controllers/WeatherForecastController.cs
@@ -26,7 +26,7 @@ namespace HelloWorld.Controllers
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)],
-                Name = "SivaKumar"
+                Name = "Narayana"
             })
             .ToArray();
         }
